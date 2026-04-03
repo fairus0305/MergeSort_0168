0b422ea3b7df3270bdeb91475be3110b170c1c85
fairus0305 <ruszezzy@gmail.com>
2026-04-03 15:51:33 +0700

    merapikan kode

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // create main array & temporary array
 int arr[20], B[20];
+// n is array input size
 int n;
 
 void input()
@@ -11,6 +12,7 @@ void input()
     {
         cout << "Masukkan Panjang element array : ";
         cin >> n;
+
         if (n <= 20)
         {
             break;
@@ -31,6 +33,7 @@ void input()
         cin >> arr[i];
     }
 }
+
 // create function mergeSort
 void mergeSort(int low, int high)
 {
@@ -38,52 +41,53 @@ void mergeSort(int low, int high)
     {
         return; // step 1.a
     }
-}
 
-int mid = (low + high) / 2; // step 2
+    int mid = (low + high) / 2; // step 2
 
-// step 3 (fungsi rekursi)
-mergeSort(low, mid);      // step 3.a
-mergeSort(mid + 1, high); // step 3.b
+    // step 3
+    // fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid);      // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
 
-// step 4 (merge)
-int i = low;     // step 4.a
-int j = mid + 1; // step 4.b
-int k = low;     // step 4.c
+    // step 4
+    int i = low;     // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low;     // step 4.c
 
-while (i <= mid && j <= high) // step 4.d
-{
-    if (arr[i] <= arr[j]) // step 4.d.i
+    while (i <= mid && j <= high) // step 4.d
     {
-        B[k] = arr[i];
-        i++;
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++; // step 4.d.ii
     }
-    else
+
+    while (j <= high) // step 4.e
     {
-        B[k] = arr[j];
-        j++;
+        B[k] = arr[j]; // step 4.e.i
+        j++;           // step 4.e.ii
+        k = k + 1;     // step 4.e.iii
     }
-    k++; // step 4.d.ii
-}
 
-while (j <= high) // step 4.e
-{
-    B[k] = arr[j];
-    j++;
-    k = k + 1;
-}
+    while (i <= mid) // step 4.f
+    {
+        B[k] = arr[i]; // step 4.f.i
+        i++;           // step 4.f.ii
+        k++;           // step 4.f.iii
+    }
 
-while (i <= mid) // step 4.f
-{
-    B[k] = arr[i]; // step 4.f.i
-    i++;           // step 4.f.ii
-    k++;           // step 4.f.iii
-}
-
-// step 5
-for (int x = low; x <= high; x++)
-{
-    arr[x] = B[x];
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
 }
 
 void output()
