5676331e706ea3eae0f37ca53ed301b2d00518a7
fairus0305 <ruszezzy@gmail.com>
2026-04-03 15:15:40 +0700

    step 3

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -41,3 +41,7 @@ void mergeSort(int low, int high)
 }
 
 int mid = (low + high) / 2; // step 2
+
+// step 3 (fungsi rekursi)
+mergeSort(low, mid);      // step 3.a
+mergeSort(mid + 1, high); // step 3.b
