85b970eccb4ce50140535e4c55042da2277a428b
fairus0305 <ruszezzy@gmail.com>
2026-04-03 15:49:38 +0700

    menampilkan hasil array setelah diurutkan

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -85,4 +85,13 @@ for (int x = low; x <= high; x++)
 {
     arr[x] = B[x];
 }
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
